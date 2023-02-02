07e93be6e0f672ebbae8d00ee5c5943d23b82c85
Fabrice Creuzot <code@luigifab.fr>
2023-02-02 20:00:00 +0000

    Version 4.4.1

--- src/app/code/community/Luigifab/Minifier/Helper/Data.php
+++ src/app/code/community/Luigifab/Minifier/Helper/Data.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created S/20/06/2015
- * Updated V/09/12/2022
+ * Updated J/05/01/2023
  *
  * Copyright 2011-2023 | Fabrice Creuzot (luigifab) <code~luigifab~fr>
  * https://github.com/luigifab/openmage-minifier
@@ -65,7 +65,7 @@ class Luigifab_Minifier_Helper_Data extends Mage_Core_Helper_Abstract {
 	}
 
 	public function getNumber($value, array $options = []) {
-		$options['locale'] = Mage::getSingleton('core/translate')->getLocale();
+		$options['locale'] = Mage::getSingleton('core/locale')->getLocaleCode();
 		return Zend_Locale_Format::toNumber($value, $options);
 	}
 
